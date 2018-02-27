d3535a88806273bdb4aa25850bbe7b19000807ac
thebernardlim <ber9000@gmail.com>
2018-02-27 12:09:35 +0800

    Updates

--- Feature/Blog/code/Controllers/ArticleController.cs
+++ Feature/Blog/code/Controllers/ArticleController.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Sitecore.Diagnostics;
 
 namespace Sitecore.Feature.Blog.Controllers
 {
@@ -15,15 +16,18 @@ namespace Sitecore.Feature.Blog.Controllers
 
         public ActionResult List()
         {
-            var currentContextItem = Sitecore.Context.Item; // Sitecore.Mvc.Presentation.RenderingContext.Current.Rendering.Item;
+            var currentContextItem = Sitecore.Context.Item; 
 
             List<Sitecore.Data.Items.Item> articleItems = currentContextItem.GetChildren().ToList();
             List<ArticleViewModel> articleViewModelList = new List<ArticleViewModel>();
+
             foreach (Sitecore.Data.Items.Item articleItem in articleItems)
             {
+                //projectExampleLogger.Info("Title: " + articleItem.Fields["Title"].Value + ", Body: " + articleItem.Fields["Body"].Value + ",Summary: " + articleItem.Fields["Summary"].Value);
+
                 ArticleViewModel articleViewModel = new ArticleViewModel()
                 {
-                    ArticleModel = new ArticleModel() { Title = articleItem.Fields["Title"].Value, Body = articleItem.Fields["Body"].Value, Summary= articleItem.Fields["Summary"].Value },
+                    ArticleModel = new ArticleModel() { Title = articleItem.Fields["ArticleTitle"].Value, Body = articleItem.Fields["ArticleBody"].Value, Summary = articleItem.Fields["ArticleSummary"].Value },
                     Url = Sitecore.Links.LinkManager.GetItemUrl(articleItem)
                 };
 
@@ -35,12 +39,19 @@ namespace Sitecore.Feature.Blog.Controllers
                 ArticleList = articleViewModelList
             };
 
-            return View();
+            return View(viewModel);
         }
 
         public ActionResult Display()
         {
-            return View();
+            var currentContextItem = Sitecore.Context.Item;
+
+            ArticleModel articleModel = new ArticleModel()
+            {
+                Body = currentContextItem.Fields["ArticleBody"].Value
+            };
+
+            return View(articleModel);
         }
     }
 }
